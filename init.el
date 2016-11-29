03f4f3ea8e9372516076c0fbcf8431f1bed7affd
mfauvel <mathieu.fauvel@ensat.fr>
2016-11-29 15:50:12 +0100

    add resize kdb

--- init.el
+++ init.el
@@ -165,6 +165,12 @@
   :config (move-text-default-bindings)
   )
 
+;; Resize window
+(global-set-key (kbd "C-c <down>") 'enlarge-window)
+(global-set-key (kbd "C-c <up>") 'shrink-window)
+(global-set-key (kbd "C-c <left>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-c <right>") 'shrink-window-horizontally)
+
 (use-package multiple-cursors
   :ensure t
   :defer t
